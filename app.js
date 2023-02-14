d1f1bec8b52bcba5fae6212dc3fb6f0581fdbe05
pedrobartolini <pedrobartolini@outlook.com>
2023-02-14 01:37:01 -0300

    haha

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-import Express from 'express'
-const app = Express()
+import steam from 'steam-user'
+import express from 'express'
 
-const port = process.env.PORT || 3001
+const port = process?.env?.PORT || 27015
+let code
 
-app.get('/', (req, res) => {
-	res.send('Hola Alberto!')
+const session = new steam()
+const credentials = {
+	accountName: process?.env?.accountName,
+	password: process?.env?.password,
+}
+session.on('loggedOn', () => {
+	console.log('logged in')
+	session.setPersona(1)
+	session.gamesPlayed([730])
 })
 
-app.listen(port, () => {
-	console.log('lo escucho Alberto')
+session.on('steamGuard', (domain, callback) => (code ? callback(code) : null))
+
+//API
+const app = express()
+app.use(express.json())
+app.post('/', (req, res) => {
+	code = req.body.steam
+	console.log('got code: ' + req.body.steam)
+	session.logOn(credentials)
+	res.send('loggin in')
 })
+
+app.listen(port, () => console.log('Escutando na porta ' + port))
