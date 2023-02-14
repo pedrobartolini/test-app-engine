5e3a3557234fa62858c50d604d28be0bbc8ebafc
pedrobartolini <pedrobartolini@outlook.com>
2023-02-14 00:45:01 -0300

    new edit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import Express from 'express'
 const app = Express()
 
+const port = process.env.PORT || 3001
+
 app.get('/', (req, res) => {
 	res.send('Hola Alberto!')
 })
 
-app.listen(443, () => {
+app.listen(port, () => {
 	console.log('lo escucho Alberto')
 })
