3248c33bc67f4a3cb6628956434b60731d76fd27
pedrobartolini <pedrobartolini@outlook.com>
2023-02-15 07:06:24 -0300

    new update

--- app.js
+++ app.js
@@ -1,40 +1,9 @@
 import * as dotenv from 'dotenv'
-import express from 'express'
-import message from './messages.js'
+import createRoutes from './routes.js'
 dotenv.config()
 
-const app = express()
-app.use(express.json())
-
-app.post('/', (req, res) => {
-	if (req.headers.authorization !== process.env.PHP_TOKEN) return res.send('no permission')
-	paciente_atualizado(req.body)
-	res.sendStatus(200)
-})
-
-app.get('/webhooks', (req, res) => res.send(req.query['hub.challenge']))
-
-app.post('/webhooks', (req, res) => {
-	const messages = req?.body?.entry[0]?.changes[0]?.value.messages
-	if (!messages) return res.sendStatus(200)
-
-	for (const each of messages) {
-		message('5532991421477').text(each.text.body)
-	}
-
-	res.sendStatus(200)
-})
-
-function paciente_atualizado(json) {
-	const options = {
-		doutor: 'Pedro',
-		paciente: json?.paciente,
-		link: json?.link,
-		status: json.status,
-	}
-	if (json.post_status !== 'publish') return
-	if (!options.paciente) return
-	message('5532991421477').text(options)
+try {
+	createRoutes()
+} catch (err) {
+	throw err
 }
-
-app.listen(process.env.PORT || 27015)
---
+++ routes.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import message from './messages.js'
+
+export default function createRoutes() {
+	const app = express()
+	app.use(express.json())
+	app.listen(process.env.PORT || 27015)
+	app.post('/updated', (req, res) => post_updated(req, res))
+	app.get('/webhooks', (req, res) => get_webhooks(req, res))
+	app.post('/webhooks', (req, res) => post_webhooks(req, res))
+}
+
+function get_webhooks(req, res) {
+	return res.send(req.query['hub.challenge'])
+}
+
+function post_updated(req, res) {
+	if (req.headers.authorization !== process.env.PHP_TOKEN) return res.sendCode(101)
+	const json = req.body
+	const options = {
+		doutor: 'Pedro',
+		paciente: json?.paciente,
+		link: json?.link,
+		status: json.status,
+	}
+	if (json.post_status !== 'publish') return res.sendCode(200)
+	if (!options.paciente) return res.sendCode(200)
+	message('5532991421477').text(options)
+	return res.sendStatus(200)
+}
+
+function post_webhooks(req, res) {
+	const messages = req?.body?.entry[0]?.changes[0]?.value.messages
+	if (!messages) return res.sendStatus(200)
+	for (const each of messages) message(each.from).text(each.text.body)
+	return res.sendStatus(200)
+}
