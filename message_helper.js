1b5b0eb520ce1afa3868852e0d0a99dc9b47e996
pedrobartolini <pedrobartolini@outlook.com>
2023-02-15 19:51:42 -0300

    dasda

--- message_helper.js
+++ message_helper.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
-export default function post_message(data) {
-	data = JSON.stringify(data)
+export default function post_message(json) {
+	const data = JSON.stringify(json)
+	console.log(json)
 	return axios({
 		method: 'post',
 		url: `https://graph.facebook.com/v15.0/109406995407357/messages`,
